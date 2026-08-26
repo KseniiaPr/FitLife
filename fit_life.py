797529fd0d1f137b2a724b1e986686c4f8f3ca57
Kseniia Protsykova <AH2421@student.jamk.fi>
2026-08-26 09:44:46 +0300

    task corrected

--- fit_life.py
+++ fit_life.py
@@ -1,17 +1,22 @@
 WATER_PER_KG = 30
 ML_IN_LITER = 1000
+SEPARATOR = '-' * 30
 
 print('Welcome to the FitLife!')
-print('-' * 30)
+print(SEPARATOR)
 
-user_name = input('What is your name? ')
+user_name = input('What is your name? ').title()
 print(
     f'Nice to meet you, {user_name}! '
     'Now answer a few questions about yourself:')
 
-user_age = int(input('How old are you? '))
-user_weight = float(input('What is your weight? (kg) '))
-user_height = float(input('What is your height? (m) '))
+user_age = int(input('How old are you? Enter your age in full years: '))
+user_weight = float(
+    input('What is your weight? (kg) ').replace(',', '.')
+)
+user_height = float(
+    input('What is your height? (m) ').replace(',', '.')
+)
 
 # body mass index calculation
 bmi = round(user_weight / (user_height ** 2), 1)
@@ -20,7 +25,7 @@ bmi = round(user_weight / (user_height ** 2), 1)
 water_ml = user_weight * WATER_PER_KG
 water_l = water_ml / ML_IN_LITER
 
-print('-' * 30)
+print(SEPARATOR)
 print(f'Report for the user: {user_name} ({user_age} y.o.)')
 print(f'Your Body Mass Index: {bmi}')
 print(f'Recommended daily water intake: {water_l:.2f} l')
